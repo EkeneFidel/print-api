ab3640d798f7f02d9aaf3e04febe2de7e3f418b2
EkeneFidel <echukwurah99@gmail.com>
2022-11-11 14:56:56 +0100

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+
 const printerRoute = require("./printer")
 
 const app = express()
+app.use(bodyParser.json());
+
 
 app.use("/print", printerRoute);
 
