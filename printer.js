c4c2eadadc118e09f13f1e8c02bb9b544776a355
EkeneFidel <echukwurah99@gmail.com>
2022-11-14 17:09:17 +0100

    commit changes

--- printer.js
+++ printer.js
@@ -6,38 +6,9 @@ const printerRoute = express.Router();
 
 printerRoute.post("/", (req, res) => {
     try {
-        const printType = req.body.printType;
-        const params = {};
-        if (printType === "kot") {
-            params.ipAddress = req.body.ipAddress;
-            params.kotId = req.body.kotId;
-            params.customer = req.body.customer;
-            params.waiter = req.body.waiter;
-            params.table = req.body.table;
-            params.area = req.body.area;
-            params.items = req.body.items;
-        }
-
-        const device = new escpos.Network(params.ipAddress, 9100);
+        const device = new escpos.Network("192.168.20.191", 9100);
         const options = { encoding: "GB18030" /* default */ };
-
         const printer = new escpos.Printer(device, options);
-
-        const itemValue = [];
-        for (item of params.items) {
-            itemValue.push({
-                text: item["item"],
-                align: "LEFT",
-                width: 0.33,
-                style: "B",
-            });
-            itemValue.push({
-                text: item["value"],
-                align: "RIGHT",
-                width: 0.33,
-                style: "B",
-            });
-        }
 
         device.open(function (error) {
             printer
@@ -45,26 +16,30 @@ printerRoute.post("/", (req, res) => {
                 .align("ct")
                 .style("bu")
                 .size(1, 1)
-                .text(`KOT ID: ${params.kotId}`)
-                .drawLine()
-                .text(`Customer: ${params.customer}`)
-                .drawLine()
-                .text(`Waiter: ${params.waiter}`)
-                .drawLine()
-                .text(`Print Area: ${params.area}`)
-                .drawLine()
-                .text(`Table: ${params.table}`)
-                .text(
-                    new Date()
-                        .toISOString()
-                        .replace(/T/, " ")
-                        .replace(/\..+/, "")
+                .text("The quick brown fox jumps over the lazy dog")
+                .text("敏捷的棕色狐狸跳过懒狗")
+                .barcode("1234567", "EAN8")
+                .table(["One", "Two", "Three"])
+                .tableCustom(
+                    [
+                        {
+                            text: "Left",
+                            align: "LEFT",
+                            width: 0.33,
+                            style: "B",
+                        },
+                        { text: "Center", align: "CENTER", width: 0.33 },
+                        { text: "Right", align: "RIGHT", width: 0.33 },
+                    ],
+                    { encoding: "cp857", size: [1, 1] } // Optional
                 )
-                .tableCustom(itemValue)
-                .qrimage("https://oyasync.app", function (err) {
-                    this.cut();
-                    this.close();
-                });
+                .qrimage(
+                    "https://github.com/song940/node-escpos",
+                    function (err) {
+                        this.cut();
+                        this.close();
+                    }
+                );
         });
         res.status(200).send("Printed kot");
     } catch (error) {
